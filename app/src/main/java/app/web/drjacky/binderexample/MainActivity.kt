08d7e83abe4b7299d47a09e803dd9db4670514e6
Drjacky <dr.jacky.2005@gmail.com>
2023-08-13 14:34:38 +0100

    Use items instead

--- app/src/main/java/app/web/drjacky/binderexample/MainActivity.kt
+++ app/src/main/java/app/web/drjacky/binderexample/MainActivity.kt
@@ -8,25 +8,24 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import app.web.drjacky.binderexample.ui.theme.BinderExampleTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import app.web.drjacky.binderexample.ui.theme.BinderExampleTheme
 import app.web.drjacky.binderexample.utils.collectIn
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -73,15 +72,15 @@ fun App(
     modifier: Modifier = Modifier,
     responseList: SnapshotStateList<String>,
 ) {
+    println("${responseList.last()} displayed")
     LazyColumn(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        itemsIndexed(responseList) { i, item ->
-            println("$i - Response $item displayed")
+        items(responseList) {
             Text(
-                text = item,
+                text = it,
                 fontSize = 16.sp,
                 textAlign = TextAlign.Center,
                 color = Color.Black
