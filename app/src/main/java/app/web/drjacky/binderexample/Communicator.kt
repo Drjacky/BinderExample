c0f4176154a5a7ffaf317c7e53655d377ce7b7da
Drjacky <dr.jacky.2005@gmail.com>
2024-07-21 14:39:38 +0100

    Working version using channel

--- app/src/main/java/app/web/drjacky/binderexample/Communicator.kt
+++ app/src/main/java/app/web/drjacky/binderexample/Communicator.kt
@@ -2,15 +2,16 @@ package app.web.drjacky.binderexample
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExecutorCoroutineDispatcher
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
 class Communicator(private val thread: ExecutorCoroutineDispatcher) {
     private val lowLevelBinder = LowLevelBinder()
-    private val responseFlow = MutableSharedFlow<String>(1)
+    private val responseChannel = Channel<String>(Channel.BUFFERED)
 
     fun start(message: String) {
         println("Start called")
@@ -18,19 +19,19 @@ class Communicator(private val thread: ExecutorCoroutineDispatcher) {
             var c = 1
             do {
                 val response = lowLevelBinder.read(message, c)
-                emitResponse(response)
+                sendResponse(response)
                 c += 1
-            } while (c <= 1)
+            } while (c <= 3)
         }
 
     }
 
-    private suspend fun emitResponse(response: String) {
-        responseFlow.emit(response)
+    private suspend fun sendResponse(response: String) {
+        responseChannel.send(response)
     }
 
     fun processResponses(): Flow<String> {
-        return responseFlow.onStart { delay(300) }
+        return responseChannel.receiveAsFlow().onStart { delay(300) }
     }
 
     fun shutdown() {
